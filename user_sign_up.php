5066d72f6566091a1917e6c7859a2547955e5d69
FranzEmsley <118352573+FranzEmsley@users.noreply.github.com>
2022-11-26 00:06:11 +0800

    Update user_sign_up.php

--- user_sign_up.php
+++ user_sign_up.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="epstyle.css">
+    <link rel="stylesheet" href="eps.css">
 </head>
 <body>
     <?php
